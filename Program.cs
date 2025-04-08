dc75af53dd490b503cc786a2a66dc846201aac7e
韩俊辉 <23161129@qq.com>
2025-04-08 14:03:14 +0800

    修改launch json文件

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
